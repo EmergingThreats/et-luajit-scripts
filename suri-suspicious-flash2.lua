6a407e2a2ffe62d42be065294d3b0b3f2d31304b
Will Metcalf <william.metcalf@gmail.com>
2014-01-27 22:33:18 -0600

    ehow/livestong flash script

--- suri-suspicious-flash2.lua
+++ suri-suspicious-flash2.lua
@@ -60,7 +60,8 @@ susp_class = {
               {"\x01\x00\x00\x00\x00\x00\xfe\x1f\xca",1,true,"Malvertising XORed Flash file #1"},
               {"\x01\x00\x00\x00\x00\x00\xce\x5f\xc5",1,true,"Malvertising XORed Flash file #2"},
               {"\x01\x00\x00\x00\x00\x00\xce\xa2\x6f",1,true,"Malvertising XORed Flash file #3"},
-              {"SharePoint.OpenDocuments.4","SharePoint.OpenDocuments.3","ms-help\x3a\x2f\x2f","location.href",4,true,"ms-help as location href likely spray attempt"}
+              {"SharePoint.OpenDocuments.4","SharePoint.OpenDocuments.3","ms-help\x3a\x2f\x2f","location.href",4,true,"ms-help as location href likely spray attempt"},
+              {"BITCH_SEARCH_RADIUS_DWORDS",1,true,"e-how/livestrong malicious SWF file"}
               --{"_doswf_package",1, true,"DoSWF encoded Flash File http://www.kahusecurity.com/2013/deobfuscating-the-ck-exploit-kit"},
              }
 
