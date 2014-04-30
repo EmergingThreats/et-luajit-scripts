64646fbff423ce5bcd3c7f05479a618a1b85c515
Will Metcalf <william.metcalf@gmail.com>
2014-04-30 14:06:40 -0500

    Add Deputy Dog Indicators

--- suri-suspicious-flash2.lua
+++ suri-suspicious-flash2.lua
@@ -79,6 +79,7 @@ susp_class = {
               {"RegExp","||||||||||||||||||||","(?i)()()(?-i)",3,true,"NuclearEK"},
               {"DoExploit","scan_for_rop_gadgets","make_vtable_rop_and_shellcode",1,true,"NuclearEK"},
               {"y5ncGlra25lcw","vial",2,true,"Goon/Fiesta"},
+              {"Tope","Pidj7gbU",2,true,"DeputyDog"},
               --{"_doswf_package",1, true,"DoSWF encoded Flash File http://www.kahusecurity.com/2013/deobfuscating-the-ck-exploit-kit"},
              }
 
