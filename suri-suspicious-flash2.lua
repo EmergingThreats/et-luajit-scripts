e56c83a1b5010a05a8b82c78c20bbfc9a1fa98f0
Will Metcalf <william.metcalf@gmail.com>
2015-02-05 17:42:01 -0600

    Flash Detection Updates tks @EKWatcher.

--- suri-suspicious-flash2.lua
+++ suri-suspicious-flash2.lua
@@ -107,7 +107,7 @@ susp_class = {
               {"p0sition","wrxeByte","wrxeMultiByte",3,true,"HanJuanEK/Angler"},
               {"sfrdtb7rJ54gRF6gL7eU8",1,true,"HanJuan/Angler"},
               {"\099\004\036\000\099\005\036\000\099\006\036\000\099\007\036\000\099\008\036\000\099\009\036\000\099\010\036\000\099\004","\099\004\045\001\099\005\045\001\099\006\045\001\099\007\045\001\099\008\045\001\099\009\045\001\099\010\036\000\099\004","\098\004\037\128\002","\036\001\197\037\255\001",3,true,"AnglerEK/HanJuan Feb 03"},
-              {"\007\000\000\000\000\000\055\098\128\147\215\021\006\000\000\000\000\000","\003\000\000\000\000\000\CWS",2,true,"2015-0311 Exploit"},
+              {"\007\000\000\000\000\000\055\098\128\147\215\021\006\000\000\000\000\000","\003\000\000\000\000\000CWS",2,true,"2015-0311 Exploit"},
               {"csvwfefdvwe","charCodeAt","Photo",3,true,"Nuclear EK Flash"},
               {"7772697465556E73696","%Wparam%W","%Weters%W",3,false,"Fiesta EK Flash"},
               --{"_doswf_package",1, true,"DoSWF encoded Flash File http://www.kahusecurity.com/2013/deobfuscating-the-ck-exploit-kit"},
@@ -427,21 +427,17 @@ function common(t,o,verbose)
                 if verbose==1 then print("Found XORed Flash header 'CWS' in binary file") end
                 return 1
             end
+            if string.sub(t,binoffset,binoffset+2) ~= "ZWS" and bit.bxor(t:byte(binoffset),t:byte(binoffset+1)) == 13 and bit.bxor(t:byte(binoffset),t:byte(binoffset+2)) == 9 then
+                if verbose==1 then print("Found XORed Flash header 'ZWS' in binary file") end
+                return 1
+            end
+            if string.sub(t,binoffset,binoffset+2) ~= "FWS" and bit.bxor(t:byte(binoffset),t:byte(binoffset+1)) == 17 and bit.bxor(t:byte(binoffset),t:byte(binoffset+2)) == 21 then
+                if verbose==1 then print("Found XORed Flash header 'FWS' in binary file") end
+                return 1
+            end
             -- Look for Embedded XOR bins
             if xor_bin_check(string.sub(t,offset + 6,offset + shortlen),verbose) == 1 then
                 return 1
-            end
-            if string.find(bindata,"doswf",0,true) ~= nil then
-                block_size = string.sub(bindata,1,1)
-	        key = string.sub(bindata,2,2)
-	        offset = struct.unpack("<I4",string.sub(bindata,3,6))
-	        length = struct.unpack("<I4",string.sub(bindata,6,9))
-                print(HexDumpString(block_size))
-                print(HexDumpString(key))
-                print(offset)
-                print(length)
-
-                --print(HexDumpString(bindata))
             end
                 
             -- Inspect Embeded Flash to a certian point. If nesting is to deep fire an event
